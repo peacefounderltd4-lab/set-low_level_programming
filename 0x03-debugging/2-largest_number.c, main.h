78f3a4d7678b592c3ee1aedce01072a57599e1ca
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-06 19:40:59 +0200

    Create 2-largest_number.c, main.h

--- 0x03-debugging/2-largest_number.c, main.h
+++ 0x03-debugging/2-largest_number.c, main.h
@@ -1,21 +1,46 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-int largest_number(int a, int b, int c)
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month (already converted to day of year)
+* @year: year
+* Return: void
+*/
+void print_remaining_days(int month, int day, int year)
 {
-    int largest = a;
+    int is_leap;
 
-    if (b > largest)
-        largest = b;
+    /* Correct leap year calculation */
+    is_leap = (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
 
-    if (c > largest)
-        largest = c;
-
-    return (largest);
+    if (is_leap)
+    {
+        if (month == 2 && day == 60)
+        {
+            /* Feb 29 is valid in a leap year */
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 366 - day);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 366 - day);
+        }
+    }
+    else
+    {
+        if (month == 2 && day == 60)
+        {
+            /* Feb 29 is invalid in a non-leap year */
+            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+        }
+        else
+        {
+            printf("Day of the year: %d\n", day);
+            printf("Remaining days: %d\n", 365 - day);
+        }
+    }
 }
