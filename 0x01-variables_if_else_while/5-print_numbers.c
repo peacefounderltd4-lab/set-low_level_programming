3905aa60ab7811b964eb5a5a306eaadf570f070f
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-06 16:39:41 +0200

    Add program to print digits 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints digits 0-9
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		printf("%d", n);
+
+	printf("\n");
+
+	return (0);
+}
