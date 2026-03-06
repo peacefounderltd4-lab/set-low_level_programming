a343a7e90c1f70c6f6dca635323d0993735e779e
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-06 19:29:48 +0200

    Create main.h

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * largest_number - returns the largest of 3 integers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+int largest_number(int a, int b, int c);
+
+#endif /* MAIN_H */
