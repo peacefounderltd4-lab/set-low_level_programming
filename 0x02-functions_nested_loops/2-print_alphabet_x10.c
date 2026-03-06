8aead3d1f1827d24af1aea987b05caf8c20e6893
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-06 18:40:33 +0200

    Refactor print_alphabet_x10 function call
    Moved the print_alphabet_x10 function call to main.

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,7 @@
 #include "main.h"
 
-/**
- * print_alphabet_x10 - prints the alphabet 10 times
- */
-void print_alphabet_x10(void)
+int main(void)
 {
-    int i;
-    char c;
-
-    for (i = 0; i < 10; i++)
-    {
-        for (c = 'a'; c <= 'z'; c++)
-            _putchar(c);
-        _putchar('\n');
-    }
+    print_alphabet_x10();
+    return (0);
 }
