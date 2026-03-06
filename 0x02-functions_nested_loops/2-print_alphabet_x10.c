f3968689f613d4772e848f8e91b3d25dffe91de9
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-06 18:38:06 +0200

    Refactor print_alphabet_x10 function documentation

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints the alphabet in lowercase 10 times
- *
- * Return: void
+ * print_alphabet_x10 - prints the alphabet 10 times
  */
 void print_alphabet_x10(void)
 {
