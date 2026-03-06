c5e0aa796eca6dffa3de29f80fd241f14288126d
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-06 17:07:05 +0200

    Create README.md with main function example

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,18 @@
 /**
  * main - prints _putchar followed by a new line
  *
- * Return: 0
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-    char str[] = "_putchar";
-    int i;
-
-    for (i = 0; str[i] != '\0'; i++)
-        _putchar(str[i]);
-
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
     _putchar('\n');
     return (0);
 }
