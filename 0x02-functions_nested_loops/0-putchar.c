cfb3598c0db72fe604936c2da7e2b289b36a7584
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-06 18:08:33 +0200

    Implement main function to print '_putchar'
    Added main function to print '_putchar' followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-
-#endif /* MAIN_H */
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
+}
