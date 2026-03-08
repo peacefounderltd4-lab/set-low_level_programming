9d0d6fe994ea56763e89fa7b1316e120687e30d7
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-08 11:16:29 +0200

    Create 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * _isalpha - checks if a character is alphabetic
+ * _isalpha - checks for alphabetic character
  * @c: character to check
  *
  * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-        return (1);
-    return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
 }
