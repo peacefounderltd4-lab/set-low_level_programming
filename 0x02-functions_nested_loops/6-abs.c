0c5c9fa5f0cff62898b6a924eb8de33886832aab
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-08 11:25:42 +0200

    Create 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,10 +5,16 @@
  * @n: integer to compute
  *
  * Return: absolute value of n
+ *
+ * Note: INT_MIN is handled by returning itself (overflow cannot be represented)
  */
 int _abs(int n)
 {
 	if (n < 0)
+	{
+		if (n == -2147483648) /* INT_MIN for 32-bit systems */
+			return (2147483647 + 1); /* works correctly in two's complement */
 		return (-n);
+	}
 	return (n);
 }
