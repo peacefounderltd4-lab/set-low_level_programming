babe59c88b7a42047e4dfb68edee2e4d87cf6ac9
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-22 08:43:45 +0200

    Create  7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * leet - encodes string
+ */
+char *leet(char *str)
+{
+    int i, j;
+    char a[] = "aAeEoOtTlL";
+    char b[] = "4433007711";
+
+    for (i = 0; str[i]; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            if (str[i] == a[j])
+                str[i] = b[j];
+        }
+    }
+
+    return (str);
+}
