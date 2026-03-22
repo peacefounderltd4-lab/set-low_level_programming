6d502be27479468fa266e0dafefac733116e0e40
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-22 08:37:32 +0200

    Create 4-rev_array.c

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses array
+ */
+void reverse_array(int *a, int n)
+{
+    int i, tmp;
+
+    for (i = 0; i < n / 2; i++)
+    {
+        tmp = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = tmp;
+    }
+}
