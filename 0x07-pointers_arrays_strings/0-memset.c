d09cf232f88fe27aa306913c5a1e475466e3bff3
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-30 16:38:38 +0200

    Refine _memset function documentation
    Updated function documentation for clarity.

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,17 @@
 #include "main.h"
 
 /**
- * _memset - fills memory with a constant byte
- * @s: pointer to memory area
- * @b: byte to fill with
- * @n: number of bytes to fill
- *
- * Return: pointer to memory area s
+ * _memset - fills memory with constant byte
+ * @s: pointer to memory
+ * @b: byte to fill
+ * @n: number of bytes
+ * Return: pointer to s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
     unsigned int i;
 
-    for (i = 0; i &lt; n; i++)
+    for (i = 0; i < n; i++)
         s[i] = b;
 
     return (s);
