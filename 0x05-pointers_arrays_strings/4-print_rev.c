b88a7062a29fe3a0bf211f0cfa6af2f6f2b1a936
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-30 17:59:19 +0200

    Simplify print_rev function implementation
    Refactor print_rev function to simplify string length calculation and reverse printing.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,28 +1,14 @@
 #include "main.h"
 
-/**
- * print_rev - prints a string in reverse, followed by a new line
- * @s: pointer to the string to print
- */
 void print_rev(char *s)
 {
-    int len = 0;
-    int i;
+    int i = 0;
 
-    if (s == NULL)
-        return;
+    while (s[i])
+        i++;
 
-    /* Find the length of the string */
-    while (s[len] != '\0')
-    {
-        len++;
-    }
-
-    /* Print the string in reverse */
-    for (i = len - 1; i >= 0; i--)
-    {
+    while (i--)
         _putchar(s[i]);
-    }
 
     _putchar('\n');
 }
