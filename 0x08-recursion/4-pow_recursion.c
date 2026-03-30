e3cc2b1cb46cd49bb5cc775e82ffd4d5bc1f8ad2
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-30 16:31:18 +0200

    Add recursive power function _pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns x^y
+ * @x: base
+ * @y: power
+ * Return: result or -1 if error
+ */
+int _pow_recursion(int x, int y)
+{
+    if (y < 0)
+        return (-1);
+
+    if (y == 0)
+        return (1);
+
+    return (x * _pow_recursion(x, y - 1));
+}
