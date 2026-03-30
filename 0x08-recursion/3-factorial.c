2264caba278439e3497847228cba4890f743c5c9
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-30 16:29:51 +0200

    Implement factorial function using recursion

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * factorial - returns factorial of n
+ * @n: number
+ * Return: factorial or -1 if error
+ */
+int factorial(int n)
+{
+    if (n < 0)
+        return (-1);
+
+    if (n == 0)
+        return (1);
+
+    return (n * factorial(n - 1));
+}
