0a3cc0bb347f8ed7c5d8db9616bfa631ae2906e1
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-03-30 16:45:06 +0200

    Add _strpbrk function for byte search in string
    Implement _strpbrk function to search for bytes in a string.

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches string for any of set of bytes
+ * @s: string
+ * @accept: set of chars
+ * Return: pointer or NULL
+ */
+char *_strpbrk(char *s, char *accept)
+{
+    int i;
+
+    while (*s)
+    {
+        for (i = 0; accept[i]; i++)
+        {
+            if (*s == accept[i])
+                return (s);
+        }
+        s++;
+    }
+
+    return (0);
+}
