5278a0946c18ce33cdbc4638f8354545d76ab90c
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-04-04 22:35:09 +0200

    Create 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,18 +1,13 @@
 #include "main.h"
 
-/**
- * print_last_digit - prints the last digit of a number
- * @n: the number to process
- *
- * Return: the value of the last digit
- */
 int print_last_digit(int n)
 {
-	int last;
+	int last = n % 10;
 
-	last = n % 10;          /* get remainder of division by 10 */
 	if (last < 0)
-		last = -last;      /* make it positive if negative */
-	_putchar('0' + last);  /* print last digit */
+		last = -last;
+
+	_putchar(last + '0');
+
 	return (last);
 }
