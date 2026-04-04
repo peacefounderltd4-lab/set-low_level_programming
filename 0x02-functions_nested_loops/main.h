35ca769092eea35b06cffe529ceedc48fccb04d1
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-04-04 22:26:16 +0200

    Create main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,21 @@
 #define MAIN_H
 
 int _putchar(char c);
+
+/* 0-9 functions */
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int print_sign(int n);
+int _isdigit(int c);
+
+/* alphabet functions */
 void print_alphabet(void);
 void print_alphabet_x10(void);
-int _islower(int c);  /* ✅ prototype must be here */
 
-#endif /* MAIN_H */
+/* advanced tasks (if included later) */
+void times_table(void);
+void add(int a, int b);
+void print_to_98(int n);
+
+#endif
