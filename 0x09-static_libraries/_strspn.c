5e406fe5d5c9a68326636ee0548a66a61d3cc754
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-04-06 23:01:33 +0200

    Create _strspn.c

---
+++ 0x09-static_libraries/_strspn.c
@@ -0,0 +1,21 @@
+unsigned int _strspn(char *s, char *accept)
+{
+    unsigned int i, j, count = 0;
+
+    for (i = 0; s[i]; i++)
+    {
+        for (j = 0; accept[j]; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                count++;
+                break;
+            }
+        }
+
+        if (!accept[j])
+            break;
+    }
+
+    return (count);
+}
