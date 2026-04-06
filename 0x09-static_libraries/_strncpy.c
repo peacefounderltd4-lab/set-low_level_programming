d25886775dffbc27bc02146bd196c751f52fed70
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-04-06 22:58:19 +0200

    Create _strncpy.c

---
+++ 0x09-static_libraries/_strncpy.c
@@ -0,0 +1,15 @@
+char *_strncpy(char *dest, char *src, int n)
+{
+    int i;
+
+    for (i = 0; i < n && src[i]; i++)
+        dest[i] = src[i];
+
+    while (i < n)
+    {
+        dest[i] = '\0';
+        i++;
+    }
+
+    return (dest);
+}
