8457521fa476c93cef9ae5c021d6e4e0a9f8b324
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-05-20 08:12:11 +0200

    Add function to count elements in listint_t list

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns number of elements in listint_t list
+ * @h: pointer to list
+ *
+ * Return: number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
