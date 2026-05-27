dad6fb58f85fb162d33abbbd057feaa2bd8c2544
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-05-27 21:27:14 +0200

    Fix sum_listint function documentation and initialization
    Updated function documentation and initialized sum variable.

--- more_singly_linked_lists/8-sum_listint.c
+++ more_singly_linked_lists/8-sum_listint.c
@@ -1,16 +1,14 @@
 #include "lists.h"
 
 /**
- * sum_listint - returns the sum of all elements in a linked list
+ * sum_listint - returns the sum of all elements in a listint_t list
  * @head: pointer to the first node
  *
- * Return: sum of all nodes
+ * Return: sum of all the data
  */
 int sum_listint(listint_t *head)
 {
-	int sum;
-
-	sum = 0;
+	int sum = 0;
 
 	while (head != NULL)
 	{
