055932538ec83045a0e306309d62ea8c7dbd6a6e
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-13 12:32:01 +0200

    Improve free_listint_safe function implementation
    Refactor free_listint_safe function for clarity and safety.

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -1,21 +1,15 @@
 #include <stdlib.h>
 #include "lists.h"
 
-/**
- * free_listint_safe - frees list safely (handles loops)
- * @h: pointer to head pointer
- *
- * Return: number of nodes freed
- */
 size_t free_listint_safe(listint_t **h)
 {
 	listint_t *tmp;
 	size_t count = 0;
 
-	if (h == NULL)
+	if (!h)
 		return (0);
 
-	while (*h != NULL)
+	while (*h)
 	{
 		tmp = (*h)->next;
 		free(*h);
