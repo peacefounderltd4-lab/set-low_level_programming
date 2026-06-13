d1034df164848017eead93ce3348ee7f89f14b37
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-13 11:30:35 +0200

    Refactor comments in insert_nodeint function
    Updated comments for clarity and consistency.

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -3,11 +3,11 @@
 
 /**
  * insert_nodeint_at_index - inserts a new node at a given index
- * @head: pointer to pointer of the first node
- * @idx: index where the new node should be added
- * @n: value of the new node
+ * @head: pointer to pointer to the first node
+ * @idx: index where the new node should be inserted
+ * @n: value to insert
  *
- * Return: address of the new node, or NULL if it failed
+ * Return: address of the new node, or NULL if it fails
  */
 listint_t *insert_nodeint_at_index(listint_t **head,
 	unsigned int idx, int n)
