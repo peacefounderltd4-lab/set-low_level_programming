7dedc2cbe886d46e6a7aa16b3dc06f181a813b9e
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-13 12:13:52 +0200

    Add function to detect loop in linked list
    Implement function to find the start of a loop in a linked list.

---
+++ more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds loop start in list
+ * @head: list head
+ *
+ * Return: node where loop starts or NULL
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow;
+	listint_t *fast;
+
+	if (head == NULL)
+		return (NULL);
+
+	slow = head;
+	fast = head;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+
+			while (slow != fast)
+			{
+				slow = slow->next;
+				fast = fast->next;
+			}
+			return (slow);
+		}
+	}
+
+	return (NULL);
+}
