45839394e4fa71021612f9c4b7106345cfba8a70
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-13 12:12:41 +0200

    Add print_listint_safe function for linked lists
    Implement a function to print a linked list safely, handling loops.

---
+++ more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - prints list safely (handles loops)
+ * @head: pointer to list
+ *
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	size_t count = 0;
+
+	if (head == NULL)
+		return (0);
+
+	slow = head;
+	fast = head;
+
+	while (fast && fast->next)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
+
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
+			return (count);
+		}
+	}
+
+	while (slow != NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
+		slow = slow->next;
+	}
+
+	return (count);
+}
