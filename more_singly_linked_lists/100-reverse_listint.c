98e47b9969d8880941ee2b61f5382c109723179b
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-13 12:29:09 +0200

    Refactor reverse_listint function for clarity

--- more_singly_linked_lists/100-reverse_listint.c
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -1,25 +1,20 @@
 #include "lists.h"
 
-/**
- * reverse_listint - reverses a linked list
- * @head: pointer to head pointer
- *
- * Return: new head
- */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *next = NULL;
+	listint_t *prev = NULL, *next = NULL, *cur;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
 
-	while (*head != NULL)
+	cur = *head;
+
+	while (cur)
 	{
-		next = (*head)->next;
-		(*head)->next = prev;
-		prev = *head;
-		*head = next;
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
 	}
 
 	*head = prev;
