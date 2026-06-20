2f88ce7979b919a31a1f27c5b4d708d8c441c3b2
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-20 14:36:42 +0200

    Refactor dlistint_len function
    Removed unnecessary comments and improved code formatting.

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,11 +1,5 @@
 #include "lists.h"
 
-/**
- * dlistint_len - returns the number of elements in a doubly linked list
- * @h: pointer to head of list
- *
- * Return: number of nodes
- */
 size_t dlistint_len(const dlistint_t *h)
 {
     size_t count = 0;
@@ -16,5 +10,5 @@ size_t dlistint_len(const dlistint_t *h)
         h = h->next;
     }
 
-    return (count);
+    return count;
 }
