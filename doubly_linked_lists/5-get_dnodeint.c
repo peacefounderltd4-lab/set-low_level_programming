1a420f41c9f2123430d3b81676657d97bb058fd2
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-20 15:05:23 +0200

    Refactor get_dnodeint_at_index function structure

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,17 +1,24 @@
 #include "lists.h"
 
+/**
+ * get_dnodeint_at_index - gets node at index
+ * @head: head of list
+ * @index: index starting from 0
+ *
+ * Return: pointer to node or NULL
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
+	unsigned int i = 0;
 
-    while (head)
-    {
-        if (i == index)
-            return head;
+	while (head)
+	{
+		if (i == index)
+			return (head);
 
-        i++;
-        head = head->next;
-    }
+		i++;
+		head = head->next;
+	}
 
-    return NULL;
+	return (NULL);
 }
