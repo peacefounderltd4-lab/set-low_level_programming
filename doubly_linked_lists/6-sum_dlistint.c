452b8fddba4a7fe39479b999d73e2cbade31368c
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-20 15:06:08 +0200

    Refactor sum_dlistint function for clarity

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,14 +1,20 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - returns sum of all n values
+ * @head: head of list
+ *
+ * Return: sum or 0
+ */
 int sum_dlistint(dlistint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 
-    while (head)
-    {
-        sum += head->n;
-        head = head->next;
-    }
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
 
-    return sum;
+	return (sum);
 }
