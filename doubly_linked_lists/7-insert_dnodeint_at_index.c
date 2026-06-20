a3bda20eac62237a090a5583986d7c2c170106fa
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-20 15:06:58 +0200

    Refactor insert_dnodeint_at_index function

--- doubly_linked_lists/7-insert_dnodeint_at_index.c
+++ doubly_linked_lists/7-insert_dnodeint_at_index.c
@@ -1,43 +1,49 @@
 #include <stdlib.h>
 #include "lists.h"
 
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
-
-dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+/**
+ * insert_dnodeint_at_index - inserts node at index
+ * @h: pointer to head
+ * @idx: index
+ * @n: value
+ *
+ * Return: new node or NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h,
+unsigned int idx, int n)
 {
-    dlistint_t *new, *tmp;
-    unsigned int i = 0;
+	dlistint_t *new, *tmp;
+	unsigned int i = 0;
 
-    if (!h)
-        return NULL;
+	if (!h)
+		return (NULL);
 
-    if (idx == 0)
-        return add_dnodeint(h, n);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    tmp = *h;
+	tmp = *h;
 
-    while (tmp && i < idx - 1)
-    {
-        tmp = tmp->next;
-        i++;
-    }
+	while (tmp && i < idx - 1)
+	{
+		tmp = tmp->next;
+		i++;
+	}
 
-    if (!tmp)
-        return NULL;
+	if (!tmp)
+		return (NULL);
 
-    new = malloc(sizeof(dlistint_t));
-    if (!new)
-        return NULL;
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
 
-    new->n = n;
-    new->next = tmp->next;
-    new->prev = tmp;
+	new->n = n;
+	new->next = tmp->next;
+	new->prev = tmp;
 
-    if (tmp->next)
-        tmp->next->prev = new;
+	if (tmp->next)
+		tmp->next->prev = new;
 
-    tmp->next = new;
+	tmp->next = new;
 
-    return new;
+	return (new);
 }
