ac53971e116e43ac5560dd1286e77928293c1da5
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-20 15:04:31 +0200

    Format free_dlistint function for consistency

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_dlistint - frees a doubly linked list
+ * @head: head of list
+ */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *tmp;
+	dlistint_t *tmp;
 
-    while (head)
-    {
-        tmp = head->next;
-        free(head);
-        head = tmp;
-    }
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
 }
