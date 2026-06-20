10928c919fee9e1f9f44e2d922ff954ead9d3a83
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-20 14:53:13 +0200

    Refactor dlistint_len for clarity and consistency

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -2,19 +2,19 @@
 
 /**
  * dlistint_len - returns the number of elements in a dlistint_t list
- * @h: pointer to head of list
+ * @h: pointer to the head of the list
  *
  * Return: number of nodes in the list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-	size_t n = 0;
+	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		n++;
+		count++;
 		h = h->next;
 	}
 
-	return (n);
+	return (count);
 }
