9d7d6a154e75e2107fee688a91805ac5db14f03f
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-20 14:45:41 +0200

    Implement function to insert node at specific index

---
+++ doubly_linked_lists/7-insert_dnodeint_at_index.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include "lists.h"
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+    dlistint_t *new, *tmp;
+    unsigned int i = 0;
+
+    if (!h)
+        return NULL;
+
+    if (idx == 0)
+        return add_dnodeint(h, n);
+
+    tmp = *h;
+
+    while (tmp && i < idx - 1)
+    {
+        tmp = tmp->next;
+        i++;
+    }
+
+    if (!tmp)
+        return NULL;
+
+    new = malloc(sizeof(dlistint_t));
+    if (!new)
+        return NULL;
+
+    new->n = n;
+    new->next = tmp->next;
+    new->prev = tmp;
+
+    if (tmp->next)
+        tmp->next->prev = new;
+
+    tmp->next = new;
+
+    return new;
+}
