b7f99354c9b674ebccd68c3ea7bfc954f94ef16c
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-06-20 15:02:22 +0200

    Simplify comments in dlistint_len function

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,16 +1,16 @@
 #include "lists.h"
 
 /**
- * dlistint_len - returns the number of elements in a dlistint_t list
- * @h: pointer to the head of the list
+ * dlistint_len - returns number of elements in list
+ * @h: pointer to head
  *
- * Return: number of nodes in the list
+ * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		count++;
 		h = h->next;
