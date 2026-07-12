00e85fd9a86ccd40b3432711cab9df8f141cf107
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-07-12 09:09:12 +0200

    Implement function to get node at specific index

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+
+		head = head->next;
+		i++;
+	}
+
+	return (NULL);
+}
