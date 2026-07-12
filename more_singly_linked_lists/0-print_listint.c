3055178a473a5e91d1d6d330bbaff82631d76997
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-07-12 08:48:37 +0200

    Change print_listint to return node count
    Modify print_listint function to return the count of nodes printed.

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include "lists.h"
 
-void print_listint(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
-	while (h != NULL)
+	size_t count = 0;
+
+	while (h)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
+		count++;
 	}
+
+	return (count);
 }
