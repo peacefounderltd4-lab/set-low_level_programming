38cab65acdc4cfe046cdaaf3f4ba2b542ebc9906
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-07-12 09:02:37 +0200

    Fix null check in listint_len function

--- more_singly_linked_lists/1-listint_len.c
+++ more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,16 @@
 #include "lists.h"
 
+/**
+ * listint_len - returns the number of elements in a listint_t list
+ * @h: pointer to the head of the list
+ *
+ * Return: number of elements
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 
-	while (h)
+	while (h != NULL)
 	{
 		count++;
 		h = h->next;
