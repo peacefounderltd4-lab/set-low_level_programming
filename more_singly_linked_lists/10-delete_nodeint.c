79e76e3ba86d5bfed760efe4c992adbe63b1866e
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-07-27 07:08:29 -0700

    Add delete_nodeint_at_index function
    Implement function to delete a node at a specified index in a singly linked list.

---
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes a node at a given index
+ * @head: pointer to the first node
+ * @index: index of the node to delete
+ *
+ * Return: 1 if successful, -1 if it fails
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current;
+	listint_t *temp;
+	unsigned int i;
+
+	/* Your code here */
+}
