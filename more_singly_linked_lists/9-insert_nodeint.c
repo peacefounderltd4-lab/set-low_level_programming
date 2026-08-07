4c9ae7c40f03071961fba7803f8dfae5f20ccc93
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-07 02:45:20 +0200

    Refactor documentation and comments in insert_nodeint
    Updated function documentation for clarity and added comments for implementation steps.

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,19 +1,41 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * insert_nodeint_at_index - inserts a node at a given index
+ * insert_nodeint_at_index - inserts a new node at a given position
  * @head: pointer to the first node
- * @idx: index where the new node is inserted
- * @n: integer stored in the new node
+ * @idx: index where the node is inserted
+ * @n: value to store in the new node
  *
- * Return: address of the new node, or NULL if it fails
+ * Return: address of new node, or NULL if it fails
  */
 listint_t *insert_nodeint_at_index(listint_t **head,
-				    unsigned int idx, int n)
+        unsigned int idx, int n)
 {
-	listint_t *new_node;
-	listint_t *current;
-	unsigned int i;
+    /* 1. Check if head pointer is valid */
 
-	/* Write your solution here */
+    /* 2. Allocate memory for a new node */
+
+    /* 3. Handle malloc failure */
+
+    /* 4. Assign value to the new node */
+
+    /* 5. If inserting at index 0:
+     *    - connect new node to current head
+     *    - update head
+     *    - return new node
+     */
+
+    /* 6. Move through the list until reaching the node
+     *    before the insertion point
+     */
+
+    /* 7. If index does not exist:
+     *    - free allocated node
+     *    - return NULL
+     */
+
+    /* 8. Link the new node into the list */
+
+    /* 9. Return the new node address */
 }
