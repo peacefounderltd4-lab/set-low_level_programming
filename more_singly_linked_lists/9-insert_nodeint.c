cf6c754f3949ea3d8bdda361ac90286a05750429
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 11:26:36 +0200

    Refactor insert_nodeint_at_index function

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,36 +1,35 @@
-```c
 #include <stdlib.h>
 #include "lists.h"
 
 /**
  * insert_nodeint_at_index - inserts a node at a given index
- * @head: pointer to the head of the list
+ * @head: pointer to the first node
  * @idx: index where the new node is inserted
  * @n: integer stored in the new node
  *
- * Return: address of the new node, or NULL if it fails
+ * Return: address of the new node, or NULL on failure
  */
 listint_t *insert_nodeint_at_index(listint_t **head,
 		unsigned int idx, int n)
 {
-	listint_t *new_node;
+	listint_t *new;
 	listint_t *current;
 	unsigned int i;
 
 	if (head == NULL)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
 
-	new_node->n = n;
+	new->n = n;
 
 	if (idx == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		new->next = *head;
+		*head = new;
+		return (new);
 	}
 
 	current = *head;
@@ -44,13 +43,12 @@ listint_t *insert_nodeint_at_index(listint_t **head,
 
 	if (current == NULL)
 	{
-		free(new_node);
+		free(new);
 		return (NULL);
 	}
 
-	new_node->next = current->next;
-	current->next = new_node;
+	new->next = current->next;
+	current->next = new;
 
-	return (new_node);
+	return (new);
 }
-```
