168d06b7d5654b0bef12cf3800b6ba2ebc85babe
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 10:43:14 +0200

    Fix reverse_listint to properly reverse linked list
    Updated the reverse_listint function to correctly reverse a listint_t linked list and handle edge cases.

--- more_singly_linked_lists/100-reverse_listint.c
+++ more_singly_linked_lists/100-reverse_listint.c
@@ -1,15 +1,30 @@
 #include "lists.h"
 
 /**
- * reverse_listint - reverses a linked list
+ * reverse_listint - reverses a listint_t linked list
  * @head: pointer to the head of the list
  *
  * Return: pointer to the first node of the reversed list
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev;
 	listint_t *current;
+	listint_t *previous;
 
-	/* Your code here */
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	current = *head;
+	previous = NULL;
+
+	while (current != NULL)
+	{
+		*head = current->next;
+		current->next = previous;
+		previous = current;
+		current = *head;
+	}
+
+	*head = previous;
+	return (*head);
 }
