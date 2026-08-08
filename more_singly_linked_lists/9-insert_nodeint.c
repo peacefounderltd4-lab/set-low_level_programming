3f2eccb33382ae87b1c74eb945f49c1a9dec5b37
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 11:03:00 +0200

    Refactor insert_nodeint_at_index for clarity

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -1,3 +1,4 @@
+```c
 #include <stdlib.h>
 #include "lists.h"
 
@@ -9,7 +10,8 @@
  *
  * Return: address of the new node, or NULL if it fails
  */
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head,
+		unsigned int idx, int n)
 {
 	listint_t *new_node;
 	listint_t *current;
@@ -18,13 +20,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
 	if (idx == 0)
 	{
-		new_node = malloc(sizeof(listint_t));
-		if (new_node == NULL)
-			return (NULL);
-
-		new_node->n = n;
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
@@ -40,15 +43,14 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 
 	if (current == NULL)
+	{
+		free(new_node);
 		return (NULL);
+	}
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
 	new_node->next = current->next;
 	current->next = new_node;
 
 	return (new_node);
 }
+```
