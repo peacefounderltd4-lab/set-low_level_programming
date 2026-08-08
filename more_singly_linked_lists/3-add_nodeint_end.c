e55fc42b0952bd08205f025401f734515288f415
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 11:37:04 +0200

    Refactor add_nodeint_end function
    Updated function documentation and improved null check for head pointer.

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,11 +1,10 @@
-```c
 #include <stdlib.h>
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a node at the end of a list
- * @head: pointer to the head of the list
- * @n: integer to put in the new node
+ * add_nodeint_end - adds a new node at the end of a list
+ * @head: pointer to the first node
+ * @n: integer to store in the new node
  *
  * Return: address of the new element, or NULL if it failed
  */
@@ -13,9 +12,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new_node;
 	listint_t *current;
-
-	if (head == NULL)
-		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
@@ -31,7 +27,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 
 	current = *head;
-
 	while (current->next != NULL)
 		current = current->next;
 
@@ -39,4 +34,3 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	return (new_node);
 }
-```
