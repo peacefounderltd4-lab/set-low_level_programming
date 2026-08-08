2d4596c2e255931c195d0d4f251aa3214e7eec02
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 10:42:05 +0200

    Implement node deletion at specified index
    Refactor delete_nodeint_at_index to handle node deletion correctly.

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,8 +1,9 @@
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * delete_nodeint_at_index - deletes a node at a given index
- * @head: pointer to the first node
+ * @head: pointer to the head of the list
  * @index: index of the node to delete
  *
  * Return: 1 if successful, -1 if it fails
@@ -10,8 +11,35 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *current;
-	listint_t *temp;
+	listint_t *next_node;
 	unsigned int i;
 
-	/* Your code here */
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		current = *head;
+		*head = current->next;
+		free(current);
+		return (1);
+	}
+
+	current = *head;
+	i = 0;
+
+	while (current != NULL && i < index - 1)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL || current->next == NULL)
+		return (-1);
+
+	next_node = current->next;
+	current->next = next_node->next;
+	free(next_node);
+
+	return (1);
 }
