592c170e5174e74c44ea3b9452d42d2ab14d8289
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 11:33:20 +0200

    Refactor free_listint_safe to handle loops

--- more_singly_linked_lists/102-free_listint_safe.c
+++ more_singly_linked_lists/102-free_listint_safe.c
@@ -2,18 +2,25 @@
 #include "lists.h"
 
 /**
- * find_loop_node - finds the meeting point of a loop
- * @head: pointer to the head of the list
+ * free_listint_safe - frees a linked list safely
+ * @h: pointer to the list head
  *
- * Return: meeting node, or NULL
+ * Return: number of nodes freed
  */
-static listint_t *find_loop_node(listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
 	listint_t *slow;
 	listint_t *fast;
+	listint_t *start;
+	listint_t *current;
+	listint_t *next;
+	size_t count;
 
-	slow = head;
-	fast = head;
+	if (h == NULL || *h == NULL)
+		return (0);
+
+	slow = *h;
+	fast = *h;
 
 	while (fast != NULL && fast->next != NULL)
 	{
@@ -21,66 +28,37 @@ static listint_t *find_loop_node(listint_t *head)
 		fast = fast->next->next;
 
 		if (slow == fast)
-			return (slow);
+			break;
 	}
 
-	return (NULL);
-}
-
-/**
- * break_loop - breaks a loop in a linked list
- * @head: head of the list
- * @meeting: meeting node
- */
-static void break_loop(listint_t *head, listint_t *meeting)
-{
-	listint_t *start;
-	listint_t *last;
-
-	start = head;
-
-	while (start != meeting)
+	if (fast != NULL && fast->next != NULL && slow == fast)
 	{
-		start = start->next;
-		meeting = meeting->next;
+		slow = *h;
+
+		while (slow != fast)
+		{
+			slow = slow->next;
+			fast = fast->next;
+		}
+
+		start = slow;
+		current = start;
+
+		while (current->next != start)
+			current = current->next;
+
+		current->next = NULL;
 	}
 
-	last = start;
-	while (last->next != start)
-		last = last->next;
-
-	last->next = NULL;
-}
-
-/**
- * free_listint_safe - frees a list safely
- * @h: pointer to the head of the list
- *
- * Return: number of nodes freed
- */
-size_t free_listint_safe(listint_t **h)
-{
-	listint_t *current;
-	listint_t *next;
-	listint_t *meeting;
-	size_t count;
-
-	if (h == NULL || *h == NULL)
-		return (0);
-
-	meeting = find_loop_node(*h);
-	if (meeting != NULL)
-		break_loop(*h, meeting);
-
-	current = *h;
 	count = 0;
+	current = *h;
 
 	while (current != NULL)
 	{
 		next = current->next;
 		free(current);
-		current = next;
 		count++;
+		current = next;
 	}
 
 	*h = NULL;
