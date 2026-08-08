6aa6004c90c87525b02075749d73780b4b111fcd
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-08 12:15:42 +0200

    Refactor print_listint to use printf

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,47 +1,21 @@
 #include "lists.h"
-
-/**
- * print_number - prints an integer
- * @n: integer to print
- */
-static void print_number(int n)
-{
-	unsigned int number;
-
-	if (n < 0)
-	{
-		_putchar('-');
-		number = (unsigned int)(-(n + 1)) + 1;
-	}
-	else
-	{
-		number = (unsigned int)n;
-	}
-
-	if (number / 10)
-		print_number(number / 10);
-
-	_putchar('0' + number % 10);
-}
+#include <stdio.h>
 
 /**
  * print_listint - prints all elements of a listint_t list
- * @h: pointer to the first node
+ * @h: pointer to the head of the list
  *
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t count;
-
-	count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		print_number(h->n);
-		_putchar('\n');
-		h = h->next;
+		printf("%d\n", h->n);
 		count++;
+		h = h->next;
 	}
 
 	return (count);
