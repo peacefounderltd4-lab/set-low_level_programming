903da29fab71724df24029de8a5501da2fd23f8c
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-10 08:48:24 +0200

    Refactor delete_nodeint_at_index function
    Updated function documentation and fixed formatting.

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
  * delete_nodeint_at_index - deletes a node at a given index
  * @head: pointer to the first node
  * @index: index of the node to delete
  *
- * Return: 1 if successful, -1 if failed
+ * Return: 1 if successful, -1 if it fails
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
@@ -25,8 +26,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	previous = NULL;
 	i = 0;
+	previous = NULL;
 
 	while (current != NULL && i < index)
 	{
