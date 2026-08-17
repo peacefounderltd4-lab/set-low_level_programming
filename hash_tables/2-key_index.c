f5677aae693b1e5d3579bc20d6ddead8b180bdf5
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-17 07:32:51 +0200

    Improve documentation and handle zero size in key_index
    Updated function documentation for clarity and added a check for size being zero.

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -1,13 +1,15 @@
 #include "hash_tables.h"
 
 /**
- * key_index - gives the index of a key
- * @key: key to get the index for
- * @size: size of the hash table array
+ * key_index - Gives the index of a key.
+ * @key: The key to find the index for.
+ * @size: The size of the array of the hash table.
  *
- * Return: index of the key
+ * Return: The index at which the key/value pair should be stored.
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
+	if (size == 0)
+		return (0);
 	return (hash_djb2(key) % size);
 }
