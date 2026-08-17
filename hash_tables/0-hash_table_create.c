38494e470029d24706348cb2793c6d8431a5a72e
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-17 07:31:29 +0200

    Improve documentation and error handling in hash_table_create

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,10 +2,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
  *
- * Return: pointer to the new hash table, or NULL on failure
+ * Return: A pointer to the newly created hash table, or NULL if it fails.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -16,17 +16,16 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	ht = malloc(sizeof(hash_table_t));
-	if (ht == NULL)
+	if (!ht)
 		return (NULL);
 
+	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
-	if (ht->array == NULL)
+	if (!ht->array)
 	{
 		free(ht);
 		return (NULL);
 	}
-
-	ht->size = size;
 
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
