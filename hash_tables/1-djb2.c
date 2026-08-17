4c0114bc94265a14364005ffc2bbe0098dcca408
Thaddee Nzayisenga <peacefounderltd4@gmail.com>
2026-08-17 07:32:11 +0200

    Refactor comments in hash_djb2 function
    Updated comments for clarity and consistency.

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - hashes a string using the djb2 algorithm
- * @str: string to hash
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
  *
- * Return: hash value
+ * Return: The calculated hash value.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
@@ -12,11 +12,9 @@ unsigned long int hash_djb2(const unsigned char *str)
 	int c;
 
 	hash = 5381;
-
 	while ((c = *str++))
 	{
-		hash = ((hash << 5) + hash) + c;
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
-
 	return (hash);
 }
